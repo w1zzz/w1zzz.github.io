c124847cc976043474f0941e9ca23b923cf19bca
Lucian Melinte <lucianmelinte92@gmail.com>
2014-11-26 03:13:58 +0000

    sad

--- app.js
+++ app.js
@@ -1,74 +1,134 @@
 (function() {
-  var  init = function() {
-    var orderForm = document.forms.order,
-        saveBtn = document.getElementById('saveOrder'),
-        saveBtnClicked = false;
+	var SuperEditor = function() {
+		var view, fileName, isDirty = false,
+		unsavedMsg = 'Unsaved changes will be lost. Are you sure?',
+		unsavedTitle = 'Discard changes';
 
-    var saveForm = function() {
+		var markDirty = function() {
+			isDirty = true;
+		};
 
-      if(!('formAction' in document.createElement('input'))) {
-        var formAction = saveBtn.getAttribute('formaction');
-        orderForm.setAttribute('action',formAction);
-      }
-      saveBtnClicked = true;
-    };
-    saveBtn.addEventListener('click',saveForm, false);
+		var markClean = function() {
+			isDirty = false;
+		};
 
-    var qtyFields = orderForm.quantity,
-        totalFields = document.getElementsByClassName('item_total'),
-        orderTotalField = document.getElementById('order_total');
+		var checkDirty = function() {
+			if(isDirty) { return unsavedMsg; }
+		};
+		window.addEventListener('beforeunload', checkDirty, false);
 
-    var formatMoney = function(value) {
-      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+		var jump = function(e) {
+			var hash = location.hash;
 
-    var calculateTotals = function() {
-      var i = 0,
-          ln = qtyFields.length,
-          itemQty = 0,
-          itemPrice = 0.00,
-          itemTotal = 0.00,
-          itemTotalMoney = '$0.00',
-          orderTotal = 0.00,
-          orderTotalMoney = '$0.00';
+			if(hash.indexOf('/') > -1) {
+				var parts = hash.split('/'),
+					fileNameEl = document.getElementById('file_name');
 
-      for(;i<ln;i++) {
-        if(!!qtyFields[i].valueAsNumber) {
-          itemQty =qtyFields[i].valueAsNumber || 0;
-        } else {
-          itemQty =parseFloat(qtyFields[i].value) || 0;
-        }
-        if(!!qtyFields[i].dataset) {
-          itemPrice =parseFloat(qtyFields[i].dataset.price);
-        } else {
-          itemPrice =parseFloat(qtyFields[i].getAttribute('data-price'));
-        }
-        itemTotal =itemQty *itemPrice;
-        itemTotalMoney = '$'+formatMoney(itemTotal.toFixed(2));
-        orderTotal +=itemTotal;
-        orderTotalMoney = '$'+formatMoney(orderTotal.toFixed(2));
+				view = parts[0].substring(1) + '-view';
+				fileName = parts[1];
+				fileNameEl.innerHTML = fileName;
+			} else {
+				if(!isDirty || confirm(unsavedMsg, unsavedTitle)) {
+					markClean();
+					view = 'browser-view';
+					if(hash != '#list') {
+						location.hash = '#list';
+					}
+				} else {
+					location.href = e.oldURL;
+				}
+			}
+			document.body.className = view;
+		};
+		jump();
 
-        if(!!totalFields[i].value) {
-          totalFields[i].value =itemTotalMoney;
-          orderTotalField.value =orderTotalMoney;
-        } else {
-          totalFields[i].innerHTML =itemTotalMoney;
-          orderTotalField.innerHTML =orderTotalMoney;
-        }
-      }
-    };
-    calculateTotals();
+		window.addEventListener('hashchange', jump, false);
 
-    var qtyListeners = function() {
-      var i = 0,
-          ln = qtyFields.length;
-      for(;i<ln;i++) {
-        qtyFields[i].addEventListener('input',calculateTotals, false);
-        qtyFields[i].addEventListener('keyup',calculateTotals, false);
-      }
-    };
-    qtyListeners();
+		var editVisualButton = document.getElementById('edit_visual'),
+			visualView = document.getElementById('file_contents_visual'),
+			visualEditor = document.getElementById('file_contents_visual_editor'),
+			visualEditorDoc = visualEditor.contentDocument,
+			editHtmlButton = document.getElementById('edit_html'),
+			htmlView = document.getElementById('file_contents_html'),
+			htmlEditor = document.getElementById('file_contents_html_editor');
+
+		visualEditorDoc.designMode = 'on';
+		visualEditorDoc.addEventListener('keyup', markDirty, false);
+		htmlEditor.addEventListener('keyup', markDirty, false);
+
+		var updateVisualEditor = function(content) {
+			visualEditorDoc.open();
+			visualEditorDoc.write(content);
+			visualEditorDoc.close();
+			visualEditorDoc.addEventListener('keyup', markDirty, false);
+		};
+
+		var updateHtmlEditor = function(content) {
+			htmlEditor.value = content;
+		};
+
+		var toggleActiveView = function() {
+			if(htmlView.style.display == 'block') {
+				editVisualButton.className = 'split_left active';
+				visualView.style.display = 'block';
+				editHtmlButton.className = 'split_right';
+				htmlView.style.display = 'none';
+				updateVisualEditor(htmlEditor.value);
+			} else {
+				editHtmlButton.className = 'split_right active';
+				htmlView.style.display = 'block';
+				editVisualButton.className = 'split_left';
+				visualView.style.display = 'none';
+
+				var x = new XMLSerializer();
+				var content = x.serializeToString(visualEditorDoc);
+				updateHtmlEditor(content);
+			}
+		}
+		editVisualButton.addEventListener('click', toggleActiveView, false);
+		editHtmlButton.addEventListener('click', toggleActiveView, false);
+
+		var visualEditorToolbar = document.getElementById('file_contents_visual_toolbar');
+		var richTextAction = function(e) {
+			var command,
+				node = (e.target.nodeName === "BUTTON") ? e.target : e.target.parentNode;
+
+			if(node.dataset) {
+				command = node.dataset.command;
+			} else {
+				command = node.getAttribute('data-command');
+			}
+
+			var doPopupCommand = function(command, promptText, promptDefault) {
+				visualEditorDoc.execCommand(command, false, prompt(promptText, promptDefault));
+			}
+
+			if(command === 'createLink') {
+				doPopupCommand(command, 'Enter link URL:', 'http://www.example.com');
+			} else if(command === 'insertImage') {
+				doPopupCommand(command, 'Enter image URL:', 'http://www.example.com/image.png');
+			} else if(command === 'insertMap') {
+				if(navigator.geolocation) {
+					node.innerHTML = 'Loading';
+					navigator.geolocation.getCurrentPosition(function(pos) {
+						var coords = pos.coords.latitude + ',' + pos.coords.longitude;
+						var img = 'http://maps.googleapis.com/maps/api/staticmap?markers=' + coords + '&zoom=11&size=200x200&sensor=false';
+						visualEditorDoc.execCommand('insertImage', false, img);
+						node.innerHTML = 'Location Map';
+					});
+				} else {
+					alert('Geolocation not available', 'No geolocation data');
+				}
+			} else {
+				visualEditorDoc.execCommand(command);
+			}
+		};
+		visualEditorToolbar.addEventListener('click', richTextAction, false);
 	};
+
+	var init = function() {
+		new SuperEditor();
+	}
 
 	window.addEventListener('load', init, false);
 })();
