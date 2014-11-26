4a5a575d81b761d88e828ffefd88fa253541fd75
Lucian Melinte <lucianmelinte92@gmail.com>
2014-11-26 00:06:40 +0000

    update

--- app.js
+++ app.js
@@ -1,77 +1,73 @@
 (function() {
-	var init = function() {
-		var orderForm = document.forms.order,
-			saveBtn = document.getElementById('saveOrder'),
-			saveBtnClicked = false;
+  var  init = function() {
+    var orderForm = document.forms.order,
+        saveBtn = document.getElementById('saveOrder'),
+        saveBtnClicked = false;
 
-		var saveForm = function() {
-			if(!('formAction' in document.createElement('input'))) {
-				var formAction = saveBtn.getAttribute('formaction');
-				orderForm.setAttribute('action', formAction);
-			}
-			saveBtnClicked = true;
-		};
-		saveBtn.addEventListener('click', saveForm, false);
+    var saveForm = function() {
 
-		var qtyFields = orderForm.quantity,
-			totalFields = document.getElementByClassName('item_total'),
-			orderTotalField = document.getElementById('order_total');
+      if(!('formAction' in document.createElement('input'))) {
+        var formAction = saveBtn.getAttribute('formaction');
+        orderForm.setAttribute('action',formAction);
+      }
+      saveBtnClicked = true;
+    };
+    saveBtn.addEventListener('click',saveForm, false);
 
-		var formatMoney = function(value) {
-			return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-		}
+    var qtyFields = orderForm.quantity,
+        totalFields = document.getElementsByClassName('item_total'),
+        orderTotalField = document.getElementById('order_total');
 
-		var calculateTotals = function() {
-			var i = 0,
-				ln = qtyFields.length,
-				itemQty = 0,
-				itemPrice = 0.00,
-				itemTotal = 0.00,
-				itemTotalMoney = '$0.00',
-				orderTotal = 0.00,
-				orderTotalMoney = '$0.00';
+    var formatMoney = function(value) {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
-			for(; i<ln; i++) {
-				if(!!qtyFields[i].valueAsNumber) {
-					itemQty = qtyFields[i].valueAsNumber || 0;
-				} else {
-					itemQty = parseFloat(qtyFields[i].value) || 0;
-				}
+    var calculateTotals = function() {
+      var i = 0,
+          ln = qtyFields.length,
+          itemQty = 0,
+          itemPrice = 0.00,
+          itemTotal = 0.00,
+          itemTotalMoney = '$0.00',
+          orderTotal = 0.00,
+          orderTotalMoney = '$0.00';
 
-				if(!!qtyFields[i].dataset) {
-					itemPrice = parseFloat(qtyFields[i].dataset.price);
-				} else {
-					itemPrice = parseFloat(qtyFields[i].getAttribute('data-price'));
-				}
+      for(;i<ln;i++) {
+        if(!!qtyFields[i].valueAsNumber) {
+          itemQty =qtyFields[i].valueAsNumber || 0;
+        } else {
+          itemQty =parseFloat(qtyFields[i].value) || 0;
+        }
+        if(!!qtyFields[i].dataset) {
+          itemPrice =parseFloat(qtyFields[i].dataset.price);
+        } else {
+          itemPrice =parseFloat(qtyFields[i].getAttribute('data-price'));
+        }
+        itemTotal =itemQty *itemPrice;
+        itemTotalMoney = '$'+formatMoney(itemTotal.toFixed(2));
+        orderTotal +=itemTotal;
+        orderTotalMoney = '$'+formatMoney(orderTotal.toFixed(2));
 
-				itemTotal = itemQty * itemPrice;
-				itemTotalMoney = '$'+formatMoney(itemTotal.toFixed(2));
-				orderTotal += itemTotal;
-				orderTotalMoney = '$'+formatMoney(orderTotal.toFixed(2));
+        if(!!totalFields[i].value) {
+          totalFields[i].value =itemTotalMoney;
+          orderTotalField.value =orderTotalMoney;
+        } else {
+          totalFields[i].innerHTML =itemTotalMoney;
+          orderTotalField.innerHTML =orderTotalMoney;
+        }
+      }
+    };
+    calculateTotals();
 
-				if(!!totalFields[i].value) {
-					totalFields[i].value = itemTotalMoney;
-					orderTotalField[i].value = orderTotalMoney;
-				} else {
-					totalFields[i].innerHTML = itemTotalMoney;
-					orderTotalField[i].innerHTML = orderTotalMoney;
-				}
-			}
-		};
-
-		calculateTotals();
-
-		var qtyListeners = function() {
-			var i = 0,
-				ln = qtyFields.length;
-
-			for(; i<ln; i++) {
-				qtyFields[i].addEventListener('input', calculateTotals, false);
-				qtyFields[i].addEventListener('keyup', calculateTotals, false);
-			}
-		};
-
-		qtyListeners();
+    var qtyListeners = function() {
+      var i = 0,
+          ln = qtyFields.length;
+      for(;i<ln;i++) {
+        qtyFields[i].addEventListener('input',calculateTotals, false);
+        qtyFields[i].addEventListener('keyup',calculateTotals, false);
+      }
+    };
+    qtyListeners();
 	};
 
 	window.addEventListener('load', init, false);
