a9e358ed3371d91722e4fbea41f90198b38e69bd
Lucian Melinte <w1zz13@gmail.com>
2022-01-18 16:36:28 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ const toggleBtn = document.querySelector('.nav-toggle');
 toggleBtn.addEventListener('click', () => {
 	header.classList.toggle('open');
 });
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+	let parent = this.parentNode;
+	parent.classList.add("focus");
+}
+
+function blurFunc() {
+	let parent = this.parentNode;
+	if (this.value == "") {
+		parent.classList.remove("focus");
+	}
+}
+
+inputs.forEach(input => {
+	input.addEventListener("focus", focusFunc);
+	input.addEventListener("blur", blurFunc);
+});
